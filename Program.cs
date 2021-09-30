e959c8bd8f6351569d5d5118dcd22bbc0be34f65
Ahmet Yasin <ahmetyasin0493@gmail.com>
2021-09-30 17:26:33 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FibonaciSayisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0;
+            int b=1;
+            int toplam=0;   
+            Console.Write("Adım sayısı giriniz => ");
+            int adim_sayisi = Convert.ToInt32(Console.ReadLine());
+            Console.Write(a + " " + b + " ");
+            for (int i=2;i<adim_sayisi;i++)
+            {
+                toplam=a+b;
+                Console.Write(toplam + " ");
+                a=b;
+                b=toplam;
+            }
+            Console.ReadLine();
+        }
+    }
+}
